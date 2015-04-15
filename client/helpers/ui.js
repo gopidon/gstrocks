9eb029d2c5d2ff0eca9a2059ec7fb4cb8d4757b5
Gopi Donthireddy <gopi.don@gmail.com>
2015-04-15 14:37:53 +0530

    add date to home page

--- client/helpers/ui.js
+++ client/helpers/ui.js
@@ -5,3 +5,8 @@ UI.registerHelper('formatTime', function(context, options) {
     if(context)
         return moment(context).format('DD-MMM-YYYY, hh:mm A');
 });
+
+UI.registerHelper('formatDate', function(context, options) {
+    if(context)
+        return moment(context).format('DD-MMM-YYYY');
+});
