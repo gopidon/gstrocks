e11dc00ca4eb3b4fedb593cdb66eb9f26addd8f5
Gopi Donthireddy <gopi.don@gmail.com>
2015-04-16 08:28:09 +0530

    add editors opinion feature

--- client/templates/dposts/dpost_item.js
+++ client/templates/dposts/dpost_item.js
@@ -11,6 +11,18 @@ Template.dpostItem.helpers(
 
 Template.dpostItem.events(
     {
+        'click #showOnHome': function(e,t) {
+            var showOnHome = e.target.checked;
+            var params = {
+                id: this._id,
+                showOnHome: showOnHome
+            };
+            Meteor.call('showOnHome', params, function(error, result) {
+                // display the error to the user and abort
+                if (error)
+                    return throwError(error.reason);
+            });
+        },
 
         'click #dPostDelButton': function(e) {
             e.preventDefault();
