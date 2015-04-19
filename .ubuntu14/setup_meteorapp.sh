7c218259ac56f56240b59d4b0c6d7595a775e616
Gopi Donthireddy <gopi.don@gmail.com>
2015-04-20 00:02:32 +0530

    updated setup_meteor bash script

--- .ubuntu14/setup_meteorapp.sh
+++ .ubuntu14/setup_meteorapp.sh
@@ -3,12 +3,15 @@ clear
 echo 'Moving to home dir...'
 cd ~
 echo 'unbundling meteor app ..'
-tar -zxf gstrocks.tar.gz
-echo 'Changing to server dir ..'
-cd /home/gstrocks/bundle/programs/server
-echo 'Doing npm install..'
-npm install
+sudo tar -zxf gstrocks.tar.gz
 echo 'Removing unwanted bcrypt dir..'
 sudo rm -r /home/gstrocks/bundle/programs/server/npm/npm-bcrypt/node_modules/bcrypt
-echo 'Restart meteor upstart service gstrocks...'
+echo 'Changing to server folder...'
+cd /home/gstrocks/bundle/programs/server
+echo 'Doing npm install bcrypt..'
+sudo npm install bcrypt
+echo 'Copying bcrypt folder from server/n_m to server/npm/npm-bcrypt/n_m ..'
+sudo cp -r /home/gstrocks/bundle/programs/server/node_modules/bcrypt /home/gstrocks/bundle/programs/server/npm/npm-bcrypt/node_modules/
+echo 'Restarting meteor upstart service gstrocks...'
 sudo service gstrocks restart
+echo 'See gstrocks upstart problems log @ /var/log/upstart/gstrocks.log..'
