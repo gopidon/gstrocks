d0f02e604a8946a5e57102e940333e3d0221662f
Gopi Donthireddy <gopi.don@gmail.com>
2015-05-09 07:41:11 +0530

    Misc

--- client/templates/dposts/dpost_item.js
+++ client/templates/dposts/dpost_item.js
@@ -22,28 +22,6 @@ Template.dpostItem.events(
                 if (error)
                     return throwError(error.reason);
             });
-        },
-
-        'click #dPostDelButton': function(e) {
-            e.preventDefault();
-            $("#delDPostModalBody").text("Delete the post "+this.title+ "?");
-            $("#delDPostModalId").val(this._id);
-            $('#dPostDelModal').modal("show");
-        },
-        'click #delDPost': function(e) {
-            e.preventDefault();
-
-
-            var currentDPostId = $("#delDPostModalId").val();
-            Meteor.call('dpostDelete', currentDPostId, function(error) {
-                $('#dPostDelModal').modal("hide");
-                // display the error to the user and abort
-                if (error)
-                    throwError(error.reason);
-
-            });
-
-
         }
 
 
--- client/templates/dposts/dposts_list.js
+++ client/templates/dposts/dposts_list.js
@@ -11,5 +11,26 @@ Template.dpostsList.events({
     'click #loginLink': function(e) {
         e.preventDefault();
         $('#loginModal').modal("show");
+    },
+    'click #dPostDelButton': function(e) {
+        e.preventDefault();
+        $("#delDPostModalBody").text("Delete the post "+this.title+ "?");
+        $("#delDPostModalId").val(this._id);
+        $('#dPostDelModal').modal("show");
+    },
+    'click #delDPost': function(e) {
+        e.preventDefault();
+
+
+        var currentDPostId = $("#delDPostModalId").val();
+        Meteor.call('dpostDelete', currentDPostId, function(error) {
+            $('#dPostDelModal').modal("hide");
+            // display the error to the user and abort
+            if (error)
+                throwError(error.reason);
+
+        });
+
+
     }
 });
