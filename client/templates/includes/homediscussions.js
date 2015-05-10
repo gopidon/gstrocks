03bb0dbdce27f284179ffc1b94c0a01819e89354
Gopi Donthireddy <gopi.don@gmail.com>
2015-05-10 14:46:09 +0530

    Misc

--- client/templates/includes/homediscussions.js
+++ client/templates/includes/homediscussions.js
@@ -2,13 +2,16 @@
  * Created by gopi on 2/2/15.
  */
 
+Template.homediscussions.onCreated(function(){
+    Meteor.subscribe('topDPosts');
+    Meteor.subscribe('latestDPosts');
+});
+
 Template.homediscussions.helpers({
     topDPosts: function(){
-        Meteor.subscribe('topDPosts');
         return DPosts.find({}, {sort: {commentsCount: -1}, limit: 5});
     },
     latestDPosts: function(){
-        Meteor.subscribe('latestDPosts');
         return DPosts.find({}, {sort: {submitted: -1}, limit: 5})
     }
 });
