1af121944f8e9f2ad43ac746a6c9e9b125e81b84
Gopi Donthireddy <gopi.don@gmail.com>
2015-05-27 13:19:49 +0530

    Misc

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -25,31 +25,32 @@ Template.postEdit.helpers(
     }
 );
 
-Template.postEdit.events({ 'submit form': function(e) {
-    e.preventDefault();
-    var currentPostId = this._id;
-    var postProperties = {
-        title: $(e.target).find('[name=title]').val(),
-        content: $(e.target).find('[name=content]').val(),
-        visible: !($(e.currentTarget).find('#invisible')[0]['checked'])
-    }
-
-    var errors = validatePost(postProperties);
-    if (errors.title || errors.content) {
-
-        return Session.set('postEditErrors', errors);
-    }
-
-    Posts.update(currentPostId, {$set: postProperties}, function(error)
-    {
-        if (error) {
-            // display the error to the user
-            throwError(error.reason);
-        } else {
-            Router.go('newPosts');
+Template.postEdit.events({
+    'submit form': function(e) {
+        e.preventDefault();
+        var currentPostId = this._id;
+        var postProperties = {
+            title: $(e.target).find('[name=title]').val(),
+            content: $(e.target).find('[name=content]').val(),
+            visible: !($(e.currentTarget).find('#invisible')[0]['checked'])
         }
-    });
-},
+
+        var errors = validatePost(postProperties);
+        if (errors.title || errors.content) {
+
+            return Session.set('postEditErrors', errors);
+        }
+
+        Posts.update(currentPostId, {$set: postProperties}, function(error)
+        {
+            if (error) {
+                // display the error to the user
+                throwError(error.reason);
+            } else {
+                Router.go('newPosts');
+            }
+        });
+    },
     'click .delete': function(e) {
         e.preventDefault();
         if (confirm("Delete this post?")) {
